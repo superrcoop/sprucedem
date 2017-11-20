babdb4ae08486b0e8ab6e9a782ea64beca495066
Jordan Cooper <superrcoop@Jordans-MacBook-Pro.local>
2017-11-20 11:54:01 -0500

    added more imgs

--- js/main.js
+++ js/main.js
@@ -1,31 +1,8 @@
-// Hello.
-//
-// This is The Scripts used for ___________ Theme
-//
-//
 
 function main() {
 
 (function () {
    'use strict';
-
-   /* ==============================================
-  	Testimonial Slider
-  	=============================================== */ 
-
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
-
     /*====================================
     Show Menu on Book
     ======================================*/
@@ -61,31 +38,6 @@ function main() {
 				        [1600, 4]
 				      ],
   	  });
-
-  	  $("#clients").owlCarousel({
-  	 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
-				      ],
-  	  });
-
-      $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
 
   	});
 
